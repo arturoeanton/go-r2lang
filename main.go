8255e4002e8dd838c9b6bb45affa2bfc7b5010b0
arturoeanton <arturoeanton@gmail.com>
2025-01-01 22:27:51 -0300

    import

--- main.go
+++ main.go
@@ -7,27 +7,17 @@ import (
 )
 
 func main() {
-	var code string
+	filename := ""
 	if len(os.Args) > 1 {
-		filename := os.Args[1]
-		data, err := os.ReadFile(filename)
-		if err != nil {
-			fmt.Printf("Error al leer el archivo %s: %v\n", filename, err)
-			os.Exit(1)
-		}
-		code = string(data)
+		filename = os.Args[1]
 	} else {
 		// intentar main.r2
 		if _, err := os.Stat("main.r2"); os.IsNotExist(err) {
 			fmt.Println("Error: Debes pasar un archivo .r2 o tener main.r2 en el directorio actual.")
 			os.Exit(1)
 		}
-		data, err := os.ReadFile("main.r2")
-		if err != nil {
-			fmt.Println("Error al leer main.r2:", err)
-			os.Exit(1)
-		}
-		code = string(data)
+
+		filename = "main.r2"
 	}
-	r2lang.RunCode(code)
+	r2lang.RunCode(filename)
 }
